a0143012c229364eae1c99c9728804d160cc2c30
mkim777 <miakim@sas.upenn.edu>
2022-03-20 19:35:41 -0400

    Cleaned up ESLint in studies.js

--- research-app/server/routes/studies.js
+++ research-app/server/routes/studies.js
@@ -1,35 +1,35 @@
-const express = require("express");
+const express = require('express');
 
 /* STUDIES ROUTES */
 // Router controlling requests starting with path /study.
 const studyRoutes = express.Router();
-const dbo = require("../db/conn");
+const dbo = require('../db/conn');
 
 // get individual study by id
-studyRoutes.route("/study/:id").get(function (req, res) {
-  let db_connect = dbo.getDb("research-app");
-  const myquery = { studyId: parseInt(req.params.id) };
-  db_connect
-      .collection("studies")
-      .findOne(myquery, function (err, result) {
-        if (err) {
-          throw err;
-        }
-        res.json(result);
-      });
+studyRoutes.route('/study/:id').get((req, res) => {
+  const dbConnect = dbo.getDb('research-app');
+  const myquery = { studyId: parseInt(req.params.id, 10) };
+  dbConnect
+    .collection('studies')
+    .findOne(myquery, (err, result) => {
+      if (err) {
+        throw err;
+      }
+      res.json(result);
+    });
 });
 
 // get list of all studies
-studyRoutes.route("/study").get(function (req, res) {
-  let db_connect = dbo.getDb("research-app");
-  
-  db_connect
-    .collection("studies")
+studyRoutes.route('/study').get((req, res) => {
+  const dbConnect = dbo.getDb('research-app');
+
+  dbConnect
+    .collection('studies')
     .find({})
-    .toArray(function (err, result) {
+    .toArray((err, result) => {
       if (err) throw err;
       res.json(result);
     });
 });
 
-module.exports = studyRoutes; 
+module.exports = studyRoutes;
