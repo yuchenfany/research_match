1d114117cdac9fa7f6ed58311d86e1ba525a1716
syzhao53 <syzhao@sas.upenn.edu>
2022-03-20 22:38:14 -0400

    added tagging front end to Profile.js

--- research-app/src/components/Profile.js
+++ research-app/src/components/Profile.js
@@ -1,11 +1,53 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
+import Select from 'react-select';
 import { useNavigate } from 'react-router-dom';
 import '../assets/index.css';
 
 function Profile() {
   const navigate = useNavigate();
+
+  const genderOptions = [
+    { label: 'Female', value: 'female' },
+    { label: 'Male', value: 'male' },
+    { label: 'Transgender', value: 'transgender' },
+    { label: 'Non-binary', value: 'nonbinary' },
+    { label: 'Other', value: 'other' },
+  ];
+
+  const allergyTags = [
+    { label: 'Tree Nuts', value: 'treeNuts' },
+    { label: 'Gluten', value: 'gluten' },
+    { label: 'Penicillin', value: 'penicillin' },
+    { label: 'Peanuts', value: 'peanuts' },
+    { label: 'Aspirin', value: 'aspirin' },
+    { label: 'Pollen', value: 'pollen' },
+  ];
+
+  const physTags = [
+    { label: 'Arthritis', value: 'arthritis' },
+    { label: 'Asthma', value: 'asthma' },
+    { label: 'Diabetes', value: 'diabetes' },
+    { label: 'Epilepsy', value: 'epilepsy' },
+    { label: 'High Blood Pressure', value: 'highBloodPressure' },
+    { label: 'Anemia', value: 'anemia' },
+  ];
+
+  const psychTags = [
+    { label: 'Generalized Anxiety Disorder', value: 'anxiety' },
+    { label: 'Bipolar Disorder', value: 'bipolar' },
+    { label: 'Depression', value: 'depression' },
+    { label: 'ADHD', value: 'adhd' },
+    { label: 'Borderline Personality Disorder', value: 'borderline' },
+    { label: 'Insomnia', value: 'insomnia' },
+  ];
+
+  const medTags = [
+    { label: 'Adderall', value: 'adderla' },
+    { label: 'Prozac', value: 'prozac' },
+    { label: 'Lexapro', value: 'lexapro' },
+  ];
 
   const goToHome = () => {
     navigate('/home');
@@ -84,18 +126,47 @@ function Profile() {
         <div className="profile-row">
           <div>Gender</div>
           <div className="dropdown">
-            <select>
-              <option>Female</option>
-              <option>Male</option>
-              <option>Transgender</option>
-              <option>Non-binary</option>
-              <option>Other</option>
-              <option>Prefer not to say</option>
-            </select>
+            <Select
+              options={genderOptions}
+              onChange={(opt) => console.log(opt.label, opt.value)}
+            />
           </div>
         </div>
         <div className="profile-row">
           <div>Allergies</div>
+          <Select
+            options={allergyTags}
+            isMulti
+            onChange={(opt) => console.log(opt.label, opt.value)}
+            className="select-tags"
+          />
+        </div>
+        <div className="profile-row">
+          <div>Medical Conditions (Physical)</div>
+          <Select
+            options={physTags}
+            isMulti
+            onChange={(opt) => console.log(opt.label, opt.value)}
+            className="select-tags"
+          />
+        </div>
+        <div className="profile-row">
+          <div>Medical Conditions (Psychological)</div>
+          <Select
+            options={psychTags}
+            isMulti
+            onChange={(opt) => console.log(opt.label, opt.value)}
+            className="select-tags"
+          />
+        </div>
+        <div className="profile-row">
+          <div>Medications</div>
+          <Select
+            options={medTags}
+            isMulti
+            onChange={(opt) => console.log(opt.label, opt.value)}
+            className="select-tags"
+          />
         </div>
         <input className="signup-button" type="submit" value="SIGN UP" onClick={goToHome} />
       </div>
