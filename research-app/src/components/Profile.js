3b7026dd14ac64f5457b0c581cc7c91238017334
syzhao53 <syzhao@sas.upenn.edu>
2022-03-20 21:46:15 -0400

    frontend elements in Profile.js

--- research-app/src/components/Profile.js
+++ research-app/src/components/Profile.js
@@ -11,10 +11,94 @@ function Profile() {
     navigate('/home');
   };
 
+  const handleAgeChange = async () => {
+    // console.log(event.target.value);
+  };
+
   return (
     <div className="Profile">
-      <div className="header"> Edit Profile </div>
-      <input className="button" type="submit" value="SIGN UP" onClick={goToHome} />
+      <div className="profile-flex">
+        <div className="header-left"> Create Profile </div>
+        <div className="profile-row">
+          <div>Age</div>
+          <input
+            className="input-field"
+            type="text"
+            id="age"
+            onChange={handleAgeChange}
+          />
+          <div>Height</div>
+          <input
+            className="input-field"
+            type="text"
+            id="age"
+            onChange={handleAgeChange}
+          />
+          <div>ft</div>
+          <input
+            className="input-field"
+            type="text"
+            id="age"
+            onChange={handleAgeChange}
+          />
+          <div>in</div>
+          <div>Weight</div>
+          <input
+            className="input-field"
+            type="text"
+            id="age"
+            onChange={handleAgeChange}
+          />
+          <div>lbs</div>
+        </div>
+        <div className="profile-row">
+          <div>Biological Sex</div>
+          <label htmlFor="form" className="radio-option">
+            <input
+              type="radio"
+              id="male"
+              value="male"
+              name="bio-sex"
+            />
+            <div>Male</div>
+          </label>
+          <label htmlFor="form" className="radio-option">
+            <input
+              type="radio"
+              id="female"
+              value="female"
+              name="bio-sex"
+            />
+            <div>Female</div>
+          </label>
+          <label htmlFor="form" className="radio-option">
+            <input
+              type="radio"
+              id="intersex"
+              value="intersex"
+              name="bio-sex"
+            />
+            <div>Intersex</div>
+          </label>
+        </div>
+        <div className="profile-row">
+          <div>Gender</div>
+          <div className="dropdown">
+            <select>
+              <option>Female</option>
+              <option>Male</option>
+              <option>Transgender</option>
+              <option>Non-binary</option>
+              <option>Other</option>
+              <option>Prefer not to say</option>
+            </select>
+          </div>
+        </div>
+        <div className="profile-row">
+          <div>Allergies</div>
+        </div>
+        <input className="signup-button" type="submit" value="SIGN UP" onClick={goToHome} />
+      </div>
     </div>
   );
 }
