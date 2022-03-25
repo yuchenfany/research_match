4ba3124da1af195a1b61b742bd0421a8dcf0da59
syzhao53 <syzhao@sas.upenn.edu>
2022-03-25 01:19:00 -0400

    fixed setUser in Login for split home screen rendering

--- research-app/src/components/ResearcherHome.js
+++ research-app/src/components/ResearcherHome.js
@@ -4,12 +4,23 @@
 import React from 'react';
 import '../assets/index.css';
 import { useNavigate } from 'react-router-dom';
+import NavBar from './NavBar';
+
 
 function ResearcherHome({ user, setUser, setStudy }) {
+  function testUser() {
+    console.log(user.username);
+    console.log(user.password);
+    console.log(user.name);
+    console.log(user.organization);
+    console.log(user.type);
+  }
+
   return (
     <div className="ResearcherProfile">
+      <NavBar user={user} />
       <div className="header-left">Researcher Home</div>
-
+      <button className="view-button" type="button" onClick={() => testUser()}>TEST</button>
     </div>
   );
 }
