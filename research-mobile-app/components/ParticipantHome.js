600e16492f28f0d56f93478a8d20e0dd155e6847
mkim777 <miakim@sas.upenn.edu>
2022-04-21 15:27:46 -0400

    Got button on ParticipantHome to work

--- research-mobile-app/components/ParticipantHome.js
+++ research-mobile-app/components/ParticipantHome.js
@@ -12,54 +12,54 @@ function ParticipantHome({ route, navigation }) { // add props user
   console.log(user);
 
   // gets list of studies that match user's tags
-  async function getStudyIds() {
-    const data = await fetch(`http://localhost:5000/record/${user.username}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    const json = await data.json();
+  // async function getStudyIds() {
+  //   const data = await fetch(`http://localhost:5000/record/${user.username}`, {
+  //     method: 'GET',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //   });
+  //   const json = await data.json();
 
-    setUser({
-      username: user.username,
-      password: user.password,
-      phys: json.phys,
-      psych: json.psych,
-      med: json.med,
-    });
-    // return json.tags; (once tags are implemented in phys)
-    const userTags = user.phys.concat(user.psych.concat(user.med));
-    console.log(userTags);
-    console.log(user.phys);
-    return userTags;
-  }
+  //   setUser({
+  //     username: user.username,
+  //     password: user.password,
+  //     phys: json.phys,
+  //     psych: json.psych,
+  //     med: json.med,
+  //   });
+  //   // return json.tags; (once tags are implemented in phys)
+  //   const userTags = user.phys.concat(user.psych.concat(user.med));
+  //   console.log(userTags);
+  //   console.log(user.phys);
+  //   return userTags;
+  // }
 
   // gets individual study by id
-  async function getStudy(studyId) {
-    const data = await fetch(`http://localhost:5000/study/tag/${studyId}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    return data.json();
-  }
+  // async function getStudy(studyId) {
+  //   const data = await fetch(`http://localhost:5000/study/tag/${studyId}`, {
+  //     method: 'GET',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //   });
+  //   return data.json();
+  // }
 
   // get all studies
-  async function getAllStudyJson() {
-    const studyIds = await getStudyIds();
-    console.log(user.username);
-    console.log(user.tags);
-    console.log(studyIds);
-    // return Promise(getStudy(studyIds[0]));
-    return Promise.all(studyIds.map((studyId) => getStudy(studyId)));
-  }
+  // async function getAllStudyJson() {
+  //   const studyIds = await getStudyIds();
+  //   console.log(user.username);
+  //   console.log(user.tags);
+  //   console.log(studyIds);
+  //   // return Promise(getStudy(studyIds[0]));
+  //   return Promise.all(studyIds.map((studyId) => getStudy(studyId)));
+  // }
 
-  useEffect(() => {
-    getAllStudyJson()
-      .then(setEnrolledStudies);
-  }, []);
+  // useEffect(() => {
+  //   getAllStudyJson()
+  //     .then(setEnrolledStudies);
+  // }, []);
 
 //   function goToStudy(studyId) {
 //     console.log(studyId);
@@ -75,46 +75,61 @@ function ParticipantHome({ route, navigation }) { // add props user
     console.log('after navigation');
   }
 
+  const testFunction = () => {
+    console.log('IN TEST FUNCTION');
+  }
+
   const styles = StyleSheet.create({
     button: {
       height: 100,
       width: 300,
+      marginBottom: 200,
     },
   });
 
   return (
-    <View>
-      <Text>WELCOME {user.username}</Text>
-      <Text>You've officially been hacked and I now know your username is: </Text>
-      <Text>{user.username} </Text> 
-      <Text>and your password is: </Text> 
-      <Text>{user.password}</Text>
-      {/* <NavBar user={user} /> */}
-      <View>
-        <Text>Eligible Studies</Text>
-        {/* <div>
-          {
-          enrolledStudies.length === 0 ? []
-            : enrolledStudies.map(
-              (studyJson) => (
-                studyJson.map(
-                  (singleStudy) => (
-                    <div key={singleStudy.studyId} className="study">
-                      <div className="study-title">{singleStudy.title}</div>
-                      <div className="study-tag">{singleStudy.tags}</div>
-                      <button className="view-button" type="button" key={singleStudy.studyId} onClick={() => goToStudy(singleStudy.studyId)}>VIEW</button>
-                    </div>
-                  ),
-                )
-              ),
-            )
-          }
-        </div> */}
-        <Text>FOR TESTING STUDY.JS</Text>
-        <Button title="GO TO STUDY" onPress={hardcodedStudy} style={styles.button} />
-      </View>
+    <View style={styles.button}>
+      <Button
+        title="GO TO STUDY"
+        onPress={() => hardcodedStudy()}
+      />
     </View>
   );
+
+  // return (
+  //   <View>
+  //     <Text>WELCOME {user.username}</Text>
+  //     <Text>You've officially been hacked and I now know your username is: </Text>
+  //     <Text>{user.username} </Text> 
+  //     <Text>and your password is: </Text> 
+  //     <Text>{user.password}</Text>
+  //     {/* <NavBar user={user} /> */}
+  //     <View>
+  //       <Text>Eligible Studies</Text>
+  //       <div>
+  //         {
+  //         enrolledStudies.length === 0 ? []
+  //           : enrolledStudies.map(
+  //             (studyJson) => (
+  //               studyJson.map(
+  //                 (singleStudy) => (
+  //                   <div key={singleStudy.studyId} className="study">
+  //                     <div className="study-title">{singleStudy.title}</div>
+  //                     <div className="study-tag">{singleStudy.tags}</div>
+  //                     <button className="view-button" type="button" key={singleStudy.studyId} onClick={() => goToStudy(singleStudy.studyId)}>VIEW</button>
+  //                   </div>
+  //                 ),
+  //               )
+  //             ),
+  //           )
+  //         }
+  //       </div>
+  //       <Text>FOR TESTING STUDY.JS</Text>
+  //       <Button title="GO TO STUDY" onPress={() => testFunction()} style={styles.button} />
+  //       <Button title="Button Testing" onPress={() => console.log('PRESSED FOR BUTTON')} />
+  //     </View>
+  //   </View>
+  // );
 }
 
 export default ParticipantHome;
