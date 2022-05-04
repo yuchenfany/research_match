1678f3e9020efd1c7dca4a86e13cd2ed80feff4e
mkim777 <miakim@sas.upenn.edu>
2022-05-04 18:22:28 -0400

    ParticipantHome styling & eslint

--- research-mobile-app/components/ParticipantHome.js
+++ research-mobile-app/components/ParticipantHome.js
@@ -2,30 +2,32 @@
 /* eslint-disable react/jsx-filename-extension */
 
 import React, { useState, useEffect } from 'react';
-import { View, Button, Text, StyleSheet } from 'react-native';
+import {
+  View, Button, Text, StyleSheet,
+} from 'react-native';
 import NavBar from './NavBar';
 
 function ParticipantHome({ route, navigation }) { // add props user
-  let { user, setUser, setStudy } = route.params;
+  const { user, setUser, setStudy } = route.params;
   const [enrolledStudies, setEnrolledStudies] = useState([]);
 
   // gets list of studies that match user's tags
 
   async function getStudyIds() {
-     const data = await fetch(`http://localhost:5000/record/${user.username}`, {
-       method: 'GET',
-       headers: {
-         'Content-Type': 'application/json',
-       },
-     });
-     const json = await data.json();
+    const data = await fetch(`http://localhost:5000/record/${user.username}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const json = await data.json();
 
-     setUser({
-       username: json.username,
-       password: json.password,
-       phys: json.phys,
-       psych: json.psych,
-       med: json.med,
+    setUser({
+      username: json.username,
+      password: json.password,
+      phys: json.phys,
+      psych: json.psych,
+      med: json.med,
       //  age: json.age,
       //  enrolled: json.enrolled,
       //  heightFeet: json.heightFeet,
@@ -35,45 +37,45 @@ function ParticipantHome({ route, navigation }) { // add props user
       //  gender: json.gender,
       //  allergies: json.allergies,
       //  type: json.type,
-     });
+    });
 
-  //   // return json.tags; (once tags are implemented in phys)
-     //const userTags = user.phys.concat(user.psych.concat(user.med));
-     return json.phys;
-   }
+    // return json.tags; (once tags are implemented in phys)
+    // const userTags = user.phys.concat(user.psych.concat(user.med));
+    return json.phys;
+  }
 
-   // gets individual study by id
-   async function getStudy(studyId) {
-     const data = await fetch(`http://localhost:5000/study/tag/${studyId}`, {
-       method: 'GET',
-       headers: {
-         'Content-Type': 'application/json',
-       },
-     });
-     return data.json();
-   }
+  // gets individual study by id
+  async function getStudy(studyId) {
+    const data = await fetch(`http://localhost:5000/study/tag/${studyId}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    return data.json();
+  }
 
   // get all studies
-   async function getAllStudyJson() {
+  async function getAllStudyJson() {
     //  setTimeout(5000);
-     const studyIds = await getStudyIds();
-     // return Promise(getStudy(studyIds[0]));
-     return Promise.all(studyIds.map((studyId) => getStudy(studyId)));
-   }
+    const studyIds = await getStudyIds();
+    // return Promise(getStudy(studyIds[0]));
+    return Promise.all(studyIds.map((studyId) => getStudy(studyId)));
+  }
 
-   useEffect(() => {
-     getAllStudyJson()
-       .then(setEnrolledStudies);
-   }, []);
+  useEffect(() => {
+    getAllStudyJson()
+      .then(setEnrolledStudies);
+  }, []);
 
   const goToStudy = (studyId) => {
-     setStudy({ studyId });
-     navigation.navigate('Study', {
-        user: user,
-        setUser: setUser,
-        studyId: studyId,
-      });
-  }
+    setStudy({ studyId });
+    navigation.navigate('Study', {
+      user,
+      setUser,
+      studyId,
+    });
+  };
 
   async function editProfile() {
     navigation.navigate('ParticipantEdit', {
@@ -86,6 +88,7 @@ function ParticipantHome({ route, navigation }) { // add props user
     container: {
       backgroundColor: '#F3F8FA',
       flex: 1,
+      padding: 20,
     },
     header: {
       fontSize: 20,
@@ -95,6 +98,25 @@ function ParticipantHome({ route, navigation }) { // add props user
       marginBottom: 20,
       color: '#103143',
     },
+    studyCard: {
+      display: 'flex',
+      flexDirection: 'row',
+      backgroundColor: '#F9FFFE',
+      borderColor: '#808A8F',
+      borderRadius: 5,
+      borderWidth: 1,
+      paddingTop: 10,
+      paddingBottom: 18,
+      paddingRight: 10,
+      paddingLeft: 10,
+      marginBottom: 10,
+      width: 600,
+    },
+    rightFlex: {
+      marginLeft: 'auto',
+      display: 'flex',
+      flexDirection: 'row',
+    },
     button: {
       width: 275,
       height: 35,
@@ -102,46 +124,63 @@ function ParticipantHome({ route, navigation }) { // add props user
       letterSpacing: 1,
       marginTop: 10,
     },
+    viewButton: {
+      right: 0,
+      width: 100,
+      height: 25,
+      fontSize: 10,
+      letterSpacing: 1,
+      marginLeft: 'auto',
+    },
+    tag: {
+      height: 30,
+      borderColor: '#808A8F',
+      borderRadius: 5,
+      borderWidth: 1,
+      padding: 5,
+      marginRight: 10,
+    },
   });
 
   return (
     <View style={styles.container}>
       <NavBar user={user} setUser={setUser} navigation={navigation} />
       <View style={styles.button}>
-        <Button title="EDIT PROFILE" color='#103143' onPress={() => editProfile()}/>
+        <Button title="EDIT PROFILE" color="#103143" onPress={() => editProfile()} />
       </View>
       <View style={styles.button}>
         <Button
           title="My Enrolled Studies"
-          color='#103143'
+          color="#103143"
           onPress={() => {
             navigation.navigate('ParticipantStudies', {
-              user: user,
-              setUser: setUser,
+              user,
+              setUser,
             });
           }}
         />
       </View>
       <Text style={styles.header}>Eligible Studies</Text>
-          <div>
-            {
+      <View>
+        {
             enrolledStudies.length === 0 ? []
               : enrolledStudies.map(
                 (studyJson) => (
                   studyJson.map(
                     (singleStudy) => (
-                      <div key={singleStudy.studyId} className="study">
-                        <div className="study-title">{singleStudy.title}</div>
-                        <div className="study-tag">{singleStudy.tags}</div>
-                        <Button style={styles.button} type="button" title="VIEW" key={singleStudy.studyId} onPress={() => goToStudy(singleStudy.studyId)} />
-                      </div>
+                      <View key={singleStudy.studyId} className="study" style={styles.studyCard}>
+                        <Text color="#103143">{singleStudy.title}</Text>
+                        <View style={styles.rightFlex}>
+                          <Text style={styles.tag} color="#103143">{singleStudy.tags}</Text>
+                          <Button style={styles.viewButton} type="button" title="VIEW" key={singleStudy.studyId} color="#103143" onPress={() => goToStudy(singleStudy.studyId)} />
+                        </View>
+                      </View>
                     ),
                   )
                 ),
               )
             }
-          </div>
-
+      </View>
     </View>
   );
 }
