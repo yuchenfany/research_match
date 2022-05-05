49ec216a50211f6ea83b04ca8720b3220da95a62
mkim777 <miakim@sas.upenn.edu>
2022-05-05 11:46:58 -0400

    conn.js eslint

--- research-mobile-app/server/db/conn.js
+++ research-mobile-app/server/db/conn.js
@@ -1,26 +1,43 @@
-const { MongoClient } = require("mongodb");
+const { MongoClient } = require('mongodb');
+
 const Db = process.env.ATLAS_URI;
 const client = new MongoClient(Db, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
- 
-var _db;
- 
+
+let mdb;
+
 module.exports = {
-  connectToServer: function (callback) {
-    client.connect(function (err, db) {
+  connectToServer: (callback) => {
+    client.connect((err, db) => {
       // Verify we got a good "db" object
-      if (db)
-      {
-        _db = db.db("research-app");
-        console.log("Successfully connected to MongoDB."); 
+      if (db) {
+        mdb = db.db('research-app');
       }
       return callback(err);
-         });
+    });
   },
- 
-  getDb: function () {
-    return _db;
-  },
+
+  getDb: () => mdb,
 };
+
+// var _db;
+
+// module.exports = {
+//   connectToServer: function (callback) {
+//     client.connect(function (err, db) {
+//       // Verify we got a good "db" object
+//       if (db)
+//       {
+//         _db = db.db("research-app");
+//         console.log("Successfully connected to MongoDB.");
+//       }
+//       return callback(err);
+//          });
+//   },
+
+//   getDb: function () {
+//     return _db;
+//   },
+// };
